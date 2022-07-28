bcde6e2e80db7136500291bb93a3c7113b53b6a1
Stanislav2705 <stasrogal25@gmail.com>
2022-07-28 23:53:38 +0300

    we

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('play', throttle(onPlay, 1000));
+
+function onPlay(event) {
+  localStorage.setItem(LOCAL_KEY, event);
+}
