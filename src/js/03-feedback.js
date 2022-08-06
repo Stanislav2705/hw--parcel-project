b1c53ba658e03c7c87a00379e50e67ec9d7583c1
Stanislav2705 <stasrogal25@gmail.com>
2022-08-06 16:21:24 +0300

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,38 +6,42 @@ const email = document.querySelector('.feedback-form input');
 
 const LOCAL_KEY = 'feedback-form-state';
 
-
 form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('input', throttle(onFormInput, 500));
 
-const formData = localStorage.getItem(LOCAL_KEY) ? JSON.parse(localStorage.getItem(LOCAL_KEY)) : {};
+const formData = localStorage.getItem(LOCAL_KEY)
+  ? JSON.parse(localStorage.getItem(LOCAL_KEY))
+  : {};
 
 populateValuesForm();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  console.log('Отправить форму');
+  console.log(JSON.parse(localStorage.getItem(LOCAL_KEY)));
 
-  evt.currentTarget.reset();
   localStorage.removeItem(LOCAL_KEY);
+  evt.currentTarget.reset();
 }
 
 function onFormInput(evt) {
-  formData(evt.target.name) = evt.target.value;
+  const {
+    target: { name, value },
+  } = evt;
+  formData[name] = value;
 
   localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
 }
 
 function populateValuesForm() {
-  const valuesForm = JSON.parse(localStorage.getItem(LOCAL_KEY));
+  const formValues = JSON.parse(localStorage.getItem(LOCAL_KEY));
 
   if (formValues) {
-        if (valuesForm.email) {
-            email.value = valuesForm.email;
-        }
-        if (valuesForm.message) {
-           textarea.value = valuesForm.message;
-        }
+    if (formValues.email) {
+      email.value = formValues.email;
     }
+    if (formValues.message) {
+      textarea.value = formValues.message;
+    }
+  }
 }
